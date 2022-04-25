4c1f6203a4693141b8c57b2b68c852f2bb87454f
Ayman Mubarak <cs.aymanmubark@gmail.com>
2022-04-26 02:55:17 +0300

    ok

--- Mu3een/Program.cs
+++ Mu3een/Program.cs
@@ -31,7 +31,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
