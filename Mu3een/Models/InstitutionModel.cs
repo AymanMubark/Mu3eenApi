491b10a0d6bb0042e37a2e2be332dcc65eda91e2
Ayman Mubarak <cs.aymanmubark@gmail.com>
2022-05-14 10:33:17 +0300

    ok

--- Mu3een/Models/InstitutionModel.cs
+++ Mu3een/Models/InstitutionModel.cs
@@ -15,11 +15,13 @@ namespace Mu3een.Models
             Phone = institution.Phone;
             Email = institution.Email;
             ImageUrl = institution.ImageUrl;
+            CreatedAt = institution.CreatedAt;
         }
         public Guid? Id { get; set; }
         public string? Name { get; set; }
         public string? Phone { get; set; }
         public string? Email { get; set; }
         public string? ImageUrl { get; set; }
+        public DateTime? CreatedAt { get; set; }
     }
 }
--- Mu3een/Models/VolunteerModel.cs
+++ Mu3een/Models/VolunteerModel.cs
@@ -7,7 +7,7 @@ namespace Mu3een.Models
         public VolunteerModel()
         {
 
-        } 
+        }
         public VolunteerModel(Volunteer model)
         {
             Id = model.Id;
@@ -17,13 +17,15 @@ namespace Mu3een.Models
             Gender = model.Gender;
             Age = model.Age;
             Points = model.Points;
+            CreatedAt = model.CreatedAt;
         }
-        public  Guid Id { get;  }
-        public string? Name { get;  }
-        public string? Phone { get;  }
-        public string? ImageUrl { get;  }
-        public Gender? Gender { get;  }
-        public int? Age { get;  }
-        public int Points { get;  } = 0;
+        public Guid Id { get; }
+        public string? Name { get; }
+        public string? Phone { get; }
+        public string? ImageUrl { get; }
+        public Gender? Gender { get; }
+        public int? Age { get; }
+        public int Points { get; } = 0;
+        public DateTime? CreatedAt { get; set; }
     }
 }
